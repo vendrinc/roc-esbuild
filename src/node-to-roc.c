55251dc573bb01a512f15e18938b209161d155ce
Richard Feldman <oss@rtfeldman.com>
2023-06-15 16:26:49 -0400

    Tweak wording on crash reports
    (they aren't always signals!)

--- src/node-to-roc.c
+++ src/node-to-roc.c
@@ -435,7 +435,7 @@ napi_value call_roc(napi_env env, napi_callback_info info) {
     char *msg = last_roc_crash_msg != NULL ? (char *)last_roc_crash_msg
                                            : strsignal(last_signal);
     char *suffix =
-        " signal caught while running the `hello` function in main.roc";
+        " encountered while running the `hello` function in main.roc";
     char *buf =
         malloc(strlen(msg) + strlen(suffix) + 1); // +1 for the null terminator
 
